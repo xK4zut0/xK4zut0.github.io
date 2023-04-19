f88cc4af96f73744d4e5484d53ffa89a73d76620
xLightRain <xlightrainx@gmail.com>
2023-04-19 19:16:39 +0200

    just a deploy test

--- user_tracking.js
+++ user_tracking.js
@@ -1,6 +1,6 @@
 import {LitElement, html} from "lit";
-//import cosmos from "@azure/cosmos";
-//import config from './config.js'
+import cosmos from "@azure/cosmos";
+import config from './config.js'
 
 export default class FormUsageTracker extends LitElement{
 
@@ -8,38 +8,38 @@ export default class FormUsageTracker extends LitElement{
         returnString: {type: String},
     };
 
-    // async init() {
-    //     const CosmosClient = cosmos.CosmosClient;
-    //     const { cosmosDatabase: {masterKey, endpoint, databaseId, containerId}} = config;
-    //     const client = new CosmosClient({ endpoint, key: masterKey });
-    //     return { database, container, client, databaseId, containerId};
-    // }
+    async init() {
+        const CosmosClient = cosmos.CosmosClient;
+        const { cosmosDatabase: {masterKey, endpoint, databaseId, containerId}} = config;
+        const client = new CosmosClient({ endpoint, key: masterKey });
+        return { database, container, client, databaseId, containerId};
+    }
 
-    // async queryItems(idParam) {
-    //     const { container, database, client, databaseId, containerId } = await this.init();
+    async queryItems(idParam) {
+        const { container, database, client, databaseId, containerId } = await this.init();
 
-    //     const querySpec = {
-    //         query: " SELECT f.forms[0].formID FROM usertracking f WHERE f.id = 'abc1'" 
-    //         };
+        const querySpec = {
+            query: " SELECT f.forms[0].formID FROM usertracking f WHERE f.id = 'abc1'" 
+            };
 
-    //         const { resources: results } = await client
-    //         .database(databaseId)
-    //         .container(containerId)
-    //         .items.query(querySpec)
-    //         .fetchAll()
+            const { resources: results } = await client
+            .database(databaseId)
+            .container(containerId)
+            .items.query(querySpec)
+            .fetchAll()
             
-    //     for (var queryResult of results) {
-    //         let resultString = JSON.stringify(queryResult)
-    //          console.log(typeof(resultString));
+        for (var queryResult of results) {
+            let resultString = JSON.stringify(queryResult)
+             console.log(typeof(resultString));
              
-    //     }
+        }
 
-    //     const event = new CustomEvent('ntx-value-change', resultString);
-    //     this.dispatchEvent(event);
+        const event = new CustomEvent('ntx-value-change', resultString);
+        this.dispatchEvent(event);
 
-    //     return resultString;
+        return resultString;
        
-    // }
+    }
 
 
     static getMetaConfig() {
@@ -60,7 +60,7 @@ export default class FormUsageTracker extends LitElement{
 
     constructor(){
         super();
-        this.returnString = "fun";
+        this.returnString = this.queryItems();
     }
 
     render() {
