bcf65a2d14e748aeebe6588ca72ed35b5d49eed6
xLightRain <xlightrainx@gmail.com>
2023-04-20 10:47:23 +0200

    added yet another testing script

---
+++ form_usage.js
@@ -0,0 +1,51 @@
+export class FormUsagePlugin {
+
+    static properties = {
+        masterKey: {type: String}, 
+        endpoint: {type: String},
+        databaseId: {type: String},
+        containerId: {type: String},        
+      }; 
+
+      static getMetaConfig() {
+        return {
+          controlName: 'Form Usage',
+          fallbackDisableSubmit: false,
+          groupName: 'User Tracking',
+          version: '1.2',
+          pluginAuthor: 'Aljoscha Power',
+          properties: {
+            masterKey: {
+              type: 'string',
+              title: 'Database  Master-Key',
+              description: 'Please enter the Master-Key of the database'
+            },
+            
+            endpoint: {
+              type: 'string',
+              title: 'Endpoint',
+              description: 'Please enter the endpoint of your azure database'
+            },
+            databaseId: {
+              title: 'Database-ID',
+              type: 'string',
+              description: 'Please enter the Id of your database'
+            },
+            containerId: {
+              title: 'Container ID',
+              type: 'string',
+              description: 'Please enter the Id of your container'
+            }   
+            
+          }
+        };
+      }
+
+      constructor(){
+        super();
+      }
+}
+
+// registering the web component
+const elementName = 'form-usage';
+customElements.define(elementName, FormUsagePlugin);
