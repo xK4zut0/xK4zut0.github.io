5314b45951f4d8ef5ff9c6038b98e087827d3be5
x4zut0 <cookiepandaa@gmail.com>
2023-04-30 15:54:19 +0200

    working build

--- moreTesting.js
+++ moreTesting.js
@@ -8,7 +8,10 @@ export class TestPlugIn extends LitElement {
     apiKey: {type: String},
     endpoint: {type: String},
     databaseId: {type: String},
-    containerId: {type: String} 
+    containerId: {type: String},
+    current_workflowid: {type: String},
+    current_formId: {type: String},
+    current_user: {type: String}
   };
   
   // return a promise for contract changes.
@@ -35,39 +38,136 @@ export class TestPlugIn extends LitElement {
         containerId: {
             title: 'containerId',
             type: 'string'
-        }         
-        
-      }
+        },
+        current_workflowid: {
+          title: 'workflowId',
+          type: 'string'
+        },
+        current_formId: {
+          title: 'formId',
+          type: 'string'
+        },
+        current_user: {
+          title: 'user',
+          type: 'string'
+        },
+        value: {
+          title: 'Aktueller Benutzer',
+          type: 'string',
+          isValueField: true, 
+        }            
+      },
+      events: ["ntx-value-change"],
     };
   }
 
+  async init() {
+    const { database } = await client.databases.createIfNotExists({ id: databaseId });
+    const { container } = await database.containers.createIfNotExists({ id: containerId });
+    return { database, container };
+  }
 
-  checkAdress() {
-         
-      
-      return html`
+  async addFormToCosmosDB(container, resources) {
+
+    const doc = resources[0];
+    const newForm = {
+      formID: current_formId,
+      user: current_user
+    };
+    doc.forms.push(newForm);
+    const { resource: updatedDoc } = await container.items.upsert(doc);
+    console.log("Dokument aktualisiert:", updatedDoc);
+}
+
+  async  addDocumentToDb(container){
+    const newDocument = {
+      id: current_workflowid,
+      forms: [
+        {
+          formID: current_formId,
+          user: current_user
+        }
+      ]
+    };
+    const { resource: createdDoc } = await container.items.create(newDocument);
+    console.log("Neues Dokument erstellt:", createdDoc);
+  }
+
+  async queryDatabase(database, container){
+
+  const querySpec = {
+    query: "SELECT * FROM c WHERE c.id = @id ",
+    parameters: [
+      { name: "@id", value: current_workflowid }
+    ]
+  };
+
+    const { resources: results } = await client
+    .database(databaseId)
+    .container(containerId)
+    .items.query(querySpec)
+    .fetchAll()
+    
+  return results;
+  }
+
+  async saveToDatabase(jsonData){
+    const {database, container} = await init();
+    
+    const item = {
+      id: documentId,
+      jsonData: JSON.stringify(jsonData)
+    };
+
+    const {resource} = await container.items.create(item);
+
+    return resource;
+
+  }
+
+
+  async manager(idParam) {
+    var formUser = "";
+    const{database, container} = await init();
+    const queryResults = await queryDatabase(database, container);
+
+    if(queryResults.length > 0) {
+      const doc = queryResults[0];
+      const formExists = doc.forms.some(form => form.formID === current_formId);
+
+      if(formExists){
+        if (form.formID == current_formId) {
+          formUser = form.user;
+        }
+        const event = new CustomEvent('ntx-value-change', formUser);
+        this.dispatchEvent(event);
+      } else {
+        addFormToCosmosDB(container, queryResults);
+      }
+    } else {
+        addDocumentToDb(container);
+    }
+    
+    return html`
                 
             <div class="plyr__video-embed" id="player">
-            <p>Sample<p/>
+            <p>${formUser}<p/>
 
       `;
-  }     
-  
-
+  } 
   
   constructor() {
     super();
-    this.checkAdress(); 
+    this.manager(); 
     
   }
 
   
 
   render() {        
-
-    
+      
     return html`       
-           ${this.checkAdress()}      
+           ${this.manager()}      
     `;     
 
   }
